bb8dff74bd74d99c0494a3043fda3fa4e92ad3da
Dangovsky <tenkovdaniil@gmail.com>
2019-05-29 23:13:42 +0300

    add color decoding, currently does not working

--- find.py
+++ find.py
@@ -4,11 +4,17 @@ from matplotlib import pyplot as plt
 from pyzbar.pyzbar import decode
 from PIL import Image
 from sklearn.cluster import KMeans
+import math
 
 step = 2
 color_threshold = 200
 
 
+# open image, send it to zbar and calculate
+# perspective transformations
+# return arrays: located qr-codes, may be incorretly rotated;
+# perspective_transforms - matrix for cv2.warpPerspective;
+# rects - rectangles in whitch qr-codes licated
 def locate_qr(file_name):
     codes = decode(Image.open(file_name))
     print(codes)
@@ -29,8 +35,8 @@ def locate_qr(file_name):
                                [code.rect.height, 0],
                                [code.rect.height, code.rect.height],
                                [0, code.rect.height]])
-
             perspective_transform = cv2.getPerspectiveTransform(pts1, pts2)
+
             located.append(cv2.warpPerspective(
                 img, perspective_transform, (code.rect.height, code.rect.height)))
             perspective_transforms.append(perspective_transform)
@@ -43,30 +49,35 @@ def locate_qr(file_name):
 def find_patternless_corner(img, pattern_y, pattern_x, dy, dx):
     pattern_size = [1, 1, 3, 1, 1]
     pattern_color = [0, 255, 0, 255, 0]
-    half_height = img.shape[1] / 2
-    size = 0
+    half_height = int(img.shape[1] / 3)
+    size = [0, 0, 0, 0, 0]
 
+    y, x = pattern_y, pattern_x
     for i in range(0, len(pattern_size)):
-        if abs(img[pattern_y, pattern_x] - pattern_color[i]) > color_threshold:
-            return [pattern_y, pattern_x]
+        if abs(img[y, x] - pattern_color[i]) > color_threshold:
+            return [y, x]
 
-        prev_y, prev_x = pattern_y, pattern_x
-        while abs(img[prev_y, prev_x]-int(img[pattern_y, pattern_x])) < color_threshold:
-            pattern_y += dy
-            pattern_x += dx
-            if abs(pattern_y - prev_y) > half_height or abs(pattern_x - prev_x) > half_height:
-                return [prev_y, prev_x]
+        prev_y, prev_x = y, x
+        while abs(img[prev_y, prev_x]-int(img[y, x])) < color_threshold:
+            y += dy
+            x += dx
+            if abs(y - pattern_y) > half_height or abs(x - pattern_x) > half_height:
+                return [y, x]
 
-        size += abs(prev_y - pattern_y) / pattern_size[i]
-    size /= len(pattern_size)
-    return size
+        size[i] += abs(prev_y - y) / pattern_size[i]
+
+    average_size = sum(size) / len(size)
+    if max(size) - min(size) > average_size:
+        return [pattern_y, pattern_x]
+    else:
+        return average_size
 
 
 # return correctly rotated QR-code and estimated block size
 def rotate_qr(img):
     size = 0
 
-    rotation_angle = 0
+    rotation_angle = None
     for y in range(step, img.shape[0], img.shape[0]-step*2):
         for x in range(step, img.shape[1], img.shape[1]-step*2):
             rows, cols = img.shape
@@ -74,6 +85,7 @@ def rotate_qr(img):
             dx = int(np.sign(rows / 2 - x) * step)
 
             tmp = find_patternless_corner(img, y, x, dy, dx)
+
             if type(tmp) is list:
                 if tmp[0] > cols / 2 and tmp[1] < rows / 2:
                     rotation_angle = 90
@@ -81,8 +93,16 @@ def rotate_qr(img):
                     rotation_angle = 180
                 elif tmp[0] < cols / 2 and tmp[1] > rows / 2:
                     rotation_angle = 270
+                else:
+                    rotation_angle = 0
             else:
                 size += tmp
+
+    if 0 == size:
+        raise RuntimeError(
+            'Block size not found, mean all corners have not patterns')
+    if rotation_angle is None:
+        raise RuntimeError('Paternless corner not found')
 
     if rotation_angle != 0:
         rotation_matrix = cv2.getRotationMatrix2D(
@@ -90,6 +110,7 @@ def rotate_qr(img):
         img = cv2.warpAffine(img, rotation_matrix, (cols, rows))
     else:
         rotation_matrix = None
+
     size /= 3
     return size, img, rotation_matrix
 
@@ -99,6 +120,7 @@ def rotate_qr(img):
 # est_size is estimated block size
 def find_block_size(img, est_size):
     pattern_corner = [int(est_size * 7), int(est_size * 7)]
+
     color = int(img[pattern_corner[0], pattern_corner[1]])
     cnt = 0
     for y in range(pattern_corner[0], img.shape[0] - pattern_corner[0], step):
@@ -113,11 +135,21 @@ def find_block_size(img, est_size):
         if abs(img[pattern_corner[0], x] - color) > color_threshold:
             cnt += 1
             color = int(img[pattern_corner[0], x])
+
     size[1] = img.shape[1] / (cnt + 13)
+
+    if img.shape[0] == img.shape[1] and size[0] != size[1]:
+        size[0] = size[1] = (size[0] + size[1]) / 2
+
     return size
 
 
-def recover_colors(color_img, size, n_clusters):
+# uses kmeans to classify colors
+# return lables - lables for each block of qr-code
+#        lables_to_bits - array, indexes - lables, values - 3bit arrays,
+#                         each can be treated as flag for having
+#                         corresponding R, G or B color on this block
+def cluster_colors(color_img, size, n_clusters):
     X = []
     x = size[1] / 2
     while(x < color_img.shape[1]):
@@ -137,7 +169,7 @@ def recover_colors(color_img, size, n_clusters):
             thresholds[color] += cluster_center[color]
         thresholds[color] /= len(kmeans.cluster_centers_)
 
-    color_to_bits = []
+    lables_to_bits = []
 
     for cluster_center in kmeans.cluster_centers_:
         tmp = []
@@ -146,45 +178,72 @@ def recover_colors(color_img, size, n_clusters):
                 tmp.append(1)
             else:
                 tmp.append(0)
-        color_to_bits.append(tmp)
+        lables_to_bits.append(tmp)
 
-    return color_to_bits, lables
+    return lables, lables_to_bits
 
 
-def lables_to_data(lables, lables_to_bits, img_shape):
-    img = Image.new('1', img_shape)
+# make image of qr-code for each dimension of lables_to_bit[0]
+# and send it to zbar
+# return data from this codes
+def recover_data_from_lables(lables, lables_to_bits, qr_shape, qr_margin):
+    qr = Image.new('1', qr_shape, 1)
+    pix = qr.load()
     data = []
 
     for color in range(0, len(lables_to_bits[0])):
-        for x in range(0, img_shape[0]):
-            for y in range(0, img_shape[1]):
-                img[x, y] = lables_to_bits[lables[(x+1)*y]][color]
-        code = decode(img)
-        data.append(code.data)
-        print(code.data)
+        i = 0
+        for x in range(qr_margin[0], qr_shape[0] - qr_margin[0]):
+            for y in range(qr_margin[1], qr_shape[1] - qr_margin[1]):
+                pix[y, x] = (lables_to_bits[lables[i]][color])
+                i += 1
+        plt.imshow(qr), plt.title('qr')
+
+        code = decode(qr)
+        if not code:
+            continue
+
+        data.append(code[0].data)
+    return data
 
 
-def prepare_img(file_name, n_clusters):
+def decode_color_qr(file_name, n_clusters):
     codes, perspective_transform, rect = locate_qr(file_name)
+    data = []
     i = 0
     for code in codes:
         size, img, rotation_matrix = rotate_qr(code)
+
         size_true = find_block_size(img, size)
+        #size_true[0], size_true[1] = round(size_true[0]), round(size_true[1])
+
+        size_true = (5, 5)
+
+        print('img shape = ', img.shape, 'true size = ',
+              img.shape[0] / (21 + 7 + 7))
+        print('block size est = ', size)
+        print('block size = ', size_true)
 
         color_img = cv2.imread(file_name)
         color_img = color_img[rect[i].top:rect[i].top+rect[i].height,
                               rect[i].left:rect[i].left+rect[i].width]
         color_img = cv2.warpPerspective(
             color_img, perspective_transform[i], (rect[i].height, rect[i].height))
-
         if rotation_matrix is not None:
             color_img = cv2.warpAffine(
                 color_img, rotation_matrix, (color_img.shape[0], color_img.shape[1]))
 
-        lables_to_bits, lables = recover_colors(
+        lables, lables_to_bits = cluster_colors(
             color_img, size_true, n_clusters)
-        lables_to_data(lables, lables_to_bits,
-                       (color_img.shape[0], color_img.shape[1]))
+
+        qr_block_size = (1, 1)
+        qr_margin = (int(qr_block_size[0] * 4),
+                     int(qr_block_size[1] * 4))  # white area around qr-code
+        qr_shape = (int(math.sqrt(len(lables)) + qr_margin[0] * 2),
+                    int(math.sqrt(len(lables)) + qr_margin[1] * 2))
+
+        data.append(recover_data_from_lables(
+            lables, lables_to_bits, qr_shape, qr_margin))
 
         i += 1
-    return imgs, sizes
+    return data
